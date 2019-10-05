cb0a78101ef1df0e29ae523da65ca77ad307affd
ziedtuihri <zied.hack@gmail.com>
2019-10-05 16:35:39 +0100

    weback

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
+  module: {
+    rules: [
+    {
+      test: /\.html$/,
+      loader: 'html-loader'
+    },
+    {
+      test: /\.js$/,
+     
+      exclude: /node_modules/
+    }
+  ]
+  }
 };
